6648f0f5c03b0b34d3c19e3ce63b7dc4a28edec6
Carson Moore <carsonmoore@mbkpro.local>
2024-07-15 16:50:13 -0600

    you'll still need to npm install

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+require("dotenv").config({ path : "./config.env"});
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT;
+
+app.listen(port, () =>{
+  console.log(`Server is running on port ${port}`);
+})
+
+app.get("/", (req, res) =>{
+  res.send("Hello world");
+})
