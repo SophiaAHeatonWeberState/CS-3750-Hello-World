9c28364f129e5682e4421d1068b135616d92134a
Sophia A Heaton <112417560+SophiaAHeatonWeberState@users.noreply.github.com>
2024-07-15 22:29:01 -0600

    Finished

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,3 +1,26 @@
-export default function helloWorld(){
-  return <h1>helloWord</h1>
-}
+import React from "react";
+ 
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
+ 
+// We import all the components we need in our app
+import Navbar from "./components/navbar";
+import RecordList from "./components/recordList";
+import Edit from "./components/edit";
+import Create from "./components/create";
+ 
+const App = () => {
+ return (
+   <div>
+    <h1>Hello World</h1>
+     <Navbar />
+     <Routes>
+       <Route exact path="/" element={<RecordList />} />
+       <Route path="/edit/:id" element={<Edit />} />
+       <Route path="/create" element={<Create />} />
+     </Routes>
+   </div>
+ );
+};
+ 
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,13 @@
-import {StrictMode} from "react";
-import { createRoot } from "react-dom/client";
-import "./styles.css"
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-import App from "./app"
-
-const root = createRoot(document.getElementById("root"));
-root.render(
-    <StrictMode>
-        <App />
-    </StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
